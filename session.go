2ae27af88daae6a657a01f442bf7bfcdd310170d
lv hongwei <lvhongwei@chinatelecom.cn>
2023-02-19 12:06:07 +0800

    [iter]New function NewSession((*Session, error))

--- session.go
+++ session.go
@@ -1,7 +1,40 @@
 package go_zabbix
 
-import "github.com/cavaliercoder/go-zabbix"
+import (
+	"crypto/tls"
+	"github.com/cavaliercoder/go-zabbix"
+	"net/http"
+)
+
+type ZabbixServer struct {
+	Addr     string `json:"address"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
 
 type Session struct {
-	InerSession *zabbix.Session
+	*zabbix.Session
+}
+
+func (this *ZabbixServer) NewSession() (*Session, error) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+
+	cache := zabbix.NewSessionFileCache().SetFilePath("./zabbix_session")
+	url := "http://" + this.Addr + "/zabbix/api_jsonrpc.php"
+	session, err := zabbix.CreateClient(url).
+		WithCache(cache).
+		WithHTTPClient(client).
+		WithCredentials(this.User, this.Password).
+		Connect()
+	if err != nil {
+		return nil, err
+	}
+	cdSession := &Session{
+		session,
+	}
+	return cdSession, nil
 }
